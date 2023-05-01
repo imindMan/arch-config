7fef86100c616f107176babbf32c2c754b912126
imindMan <khanhdonglethanhtong7b2@gmail.com>
2023-05-01 14:05:42 +0700

    Fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+starship init fish | source
+
+export EDITOR="nvim"
+set -x PATH ~/.local/bin $PATH
+alias ls='ls --color=auto'
+alias fls='ls | lolcat'
+
+alias clock='tty-clock -s -S -c -C 4'
+alias music='ncmpcpp'
+
+
