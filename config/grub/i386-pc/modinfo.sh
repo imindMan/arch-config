1373a1d62da7fbe5c11f31a11a8fa7d1198bb5f2
imindMan <khanhdonglethanhtong7b2@gmail.com>
2023-08-27 14:55:50 +0700

    Update 3

--- config/grub/i386-pc/modinfo.sh
+++ config/grub/i386-pc/modinfo.sh
@@ -14,9 +14,9 @@ grub_end_symbol="end"
 
 # Build environment
 grub_target_cc='gcc'
-grub_target_cc_version='gcc (GCC) 12.2.0'
+grub_target_cc_version='gcc (GCC) 12.2.1 20230201'
 grub_target_cflags='-std=gnu99 -fno-common -Os -m32 -Wall -W -Wshadow -Wpointer-arith -Wundef -Wchar-subscripts -Wcomment -Wdeprecated-declarations -Wdisabled-optimization -Wdiv-by-zero -Wfloat-equal -Wformat-extra-args -Wformat-security -Wformat-y2k -Wimplicit -Wimplicit-function-declaration -Wimplicit-int -Wmain -Wmissing-braces -Wmissing-format-attribute -Wmultichar -Wparentheses -Wreturn-type -Wsequence-point -Wshadow -Wsign-compare -Wswitch -Wtrigraphs -Wunknown-pragmas -Wunused -Wunused-function -Wunused-label -Wunused-parameter -Wunused-value  -Wunused-variable -Wwrite-strings -Wnested-externs -Wstrict-prototypes -g -Wredundant-decls -Wmissing-prototypes -Wmissing-declarations  -Wextra -Wattributes -Wendif-labels -Winit-self -Wint-to-pointer-cast -Winvalid-pch -Wmissing-field-initializers -Wnonnull -Woverflow -Wvla -Wpointer-to-int-cast -Wstrict-aliasing -Wvariadic-macros -Wvolatile-register-var -Wpointer-sign -Wmissing-include-dirs -Wmissing-prototypes -Wmissing-declarations -Wformat=2 -march=i386 -mrtd -mregparm=3 -falign-functions=1 -falign-loops=1 -falign-jumps=1 -freg-struct-return -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow -Wa,-mx86-used-note=no -msoft-float -fno-dwarf2-cfi-asm -mno-stack-arg-probe -fno-asynchronous-unwind-tables -fno-unwind-tables -fno-ident -fno-PIE -fno-pie -fno-stack-protector -Wtrampolines'
-grub_target_cppflags=' -Wall -W  -DGRUB_MACHINE_PCBIOS=1 -DGRUB_MACHINE=I386_PC -m32 -nostdinc -isystem /usr/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include -I$(top_srcdir)/include -I$(top_builddir)/include'
+grub_target_cppflags=' -Wall -W  -DGRUB_MACHINE_PCBIOS=1 -DGRUB_MACHINE=I386_PC -m32 -nostdinc -isystem /usr/lib/gcc/x86_64-pc-linux-gnu/12.2.1/include -I$(top_srcdir)/include -I$(top_builddir)/include'
 grub_target_ccasflags=' -m32 -g  -Wa,-mx86-used-note=no -msoft-float -fno-PIE -fno-pie'
 grub_target_ldflags=' -m32 -Wl,-melf_i386 -no-pie -Wl,--build-id=none'
 grub_cflags=''
@@ -35,6 +35,6 @@ grub_target_img_ldflags='@TARGET_IMG_BASE_LDFLAGS@'
 grub_version="2.11"
 grub_package="grub"
 grub_package_string="GRUB 2.11"
-grub_package_version="2:2.06.r380.g151467888-1"
+grub_package_version="2:2.06.r499.ge67a551a4-1"
 grub_package_name="GRUB"
 grub_package_bugreport="bug-grub@gnu.org"
