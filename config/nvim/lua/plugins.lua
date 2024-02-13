a5e0f5ea992393092efea99aea11b3190fd91472
imindMan <khanhdonglethanhtong7b2@gmail.com>
2024-02-13 20:35:31 +0700

    Changing for the rice

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -214,4 +214,22 @@ return require("lazy").setup({
 			},
 		},
 	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 })
